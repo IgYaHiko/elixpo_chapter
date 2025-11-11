963c370e23056e539a1ef5c5974dd177dee99948
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2025-11-11 09:56:49 +0530

    transformed types to .py

--- elixpo.toon.encoder/python/types.py
+++ elixpo.toon.encoder/python/types.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+from typing import Optional
 @dataclass
 class Depth : int
 
@@ -10,3 +10,33 @@ class ParsedLine:
     indent: int
     content: str
     line_number: int
+
+#dummy class to represent delimeter info
+@dataclass
+class Delimeter:
+    prefix: str
+    suffix: str
+@dataclass
+class ArrayHeaderInfo:
+    key: Optional[str] = None
+    length: int 
+    delimiter: Delimeter
+    fields: Optional[list[str]] = None
+    hasLengthMarker: bool
+
+@dataclass
+class BlankLineInfo:
+    lineNumber: int 
+    indent: int 
+    depth: Depth
+
+@dataclass
+class DecodeOptions:
+    indent: Optional[int] = 2
+    strict: Optional[bool] = True
+
+@dataclass
+class EncodeOptions:
+    indent: Optional[int] = 2
+    delimeter: Optional[Delimeter] = ','
+    lengthMarker: Optional[bool] = '#' or False
